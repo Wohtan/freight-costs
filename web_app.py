0074d0eb3c21ef2c882371259e6254c6a14e0c88
Wohtan <santiagojurado377@hotmail.com>
2022-02-07 09:31:41 -0600

    Added navbar

--- web_app.py
+++ web_app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request, url_for,render_template,jsonify
+from flask import Flask, redirect, request, url_for,render_template,jsonify
 from freight_costs import calculate_costs
 from forms import Input_form
 
 app = Flask(__name__)
 app.secret_key = 'secr3t k3y'
 
-@app.route('/')
+@app.route('/',methods=["POST",'GET'])
 def home():
     form = Input_form()   
     fields = form.fields
+    input_data = request.form.to_dict()
+
+    if form.validate_on_submit():       
+        return jsonify(input_data)
 
     return render_template('calculate.html', form = form, fields = fields)
-
 
 @app.route('/calculate')
 def calculate():
