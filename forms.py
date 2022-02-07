825e60d5adf13088e107140234876fe8826dca57
Wohtan <santiagojurado377@hotmail.com>
2022-02-06 19:37:28 -0600

    Forms.py file created

---
+++ forms.py
@@ -0,0 +1,87 @@
+from random import choices
+from flask_wtf import FlaskForm
+
+from wtforms import (StringField, 
+DecimalField, 
+SelectField,
+SubmitField)
+
+from wtforms.validators import (
+    DataRequired,
+    NumberRange,
+)
+
+class Input_form(FlaskForm):
+    weight = DecimalField(
+        'Weight',
+            [DataRequired(),
+            NumberRange(min=0.05,
+            max = 50,
+            message = 'Value out of range'
+            )],
+        places= 1,
+    )
+
+    height = DecimalField(
+        'Height',
+            [DataRequired(),
+            NumberRange(min=5,
+            max = 150,
+            message = 'Value out of range'
+            )],
+        places= 1,
+    )  
+    
+    length = DecimalField(
+        'Length',
+            [DataRequired(),
+            NumberRange(min=5,
+            max = 150,
+            message = 'Value out of range'
+            )],
+        places= 1,
+    )
+
+    depth = DecimalField(
+        'Depth',
+            [DataRequired(),
+            NumberRange(min=5,
+            max = 150,
+            message = 'Value out of range'
+            )],
+        places= 1,
+    )       
+
+    value = DecimalField(
+        'EXW/FCA Value',
+            [DataRequired(),
+            NumberRange(min=1,
+            max = 500000,
+            message = 'Value out of range'
+            )],
+        places= 1,
+    )     
+
+    currency = SelectField(
+        'Currency',
+            [DataRequired()],
+        choices=[
+            ('EUR'),
+            ('MXN'),
+            ('CHF'),
+            ('USD')
+        ]
+    )    
+
+    taxes = SelectField(
+        'IGI',
+            [DataRequired()],
+        choices=[
+            ('5%'),
+            ('0%'),
+            ('10%'),
+            ('15%')
+        ]
+    )     
+
+    submit = SubmitField('Submit')  
